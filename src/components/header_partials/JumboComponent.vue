5ca841b7f7214533317d08ca76b03cb4a68c9cc1
mattialicausi <licausivitomattia@gmail.com>
2023-02-09 17:15:38 +0100

    creato jumbo e navbar component

---
+++ src/components/header_partials/JumboComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Jumbo</h2>
+</template>
+
+<script>
+    export default {
+        name: 'JumboComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+ @import '../../assets/styles/general.scss';
+ @import '../../assets/styles/partials/variables';
+
+</style>
---
+++ src/components/header_partials/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Nav bar</h2>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+    }
+</script>
+
+<style lang="scss" scoped>
+ @import '../../assets/styles/general.scss';
+ @import '../../assets/styles/partials/variables';
+
+</style>
