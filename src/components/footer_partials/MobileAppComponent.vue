f73f42a9473b64fdc8129bdb066364271e002136
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-10 17:22:35 +0100

    fix mobile section and jumbo

--- src/components/footer_partials/MobileAppComponent.vue
+++ src/components/footer_partials/MobileAppComponent.vue
@@ -1,89 +1,73 @@
 <template>
-    <div class="container container-smathphone">
-        <div class="row d-flex align-items-center">
-            <div class="col-sm-6 col-md-6 col-lg-6">
-                <img class="img-fluid" src="../../../public/image/phone-img.png" alt="phone image">
+    <div class="container-smartphone container">
+        <div class="row align-items-center">
+            <div class="col-sm-12 col-md-6 col-lg-6">
+                <img class="phone" src="image/phone-img.png" alt="phone image">
             </div>
-            <div class="col-sm-6 col-md-6 col-lg-6">
-                <div class="row">
-                    <h2 class="text-center">Provalo anche su smartphone</h2>
-                    <p class="text-center">Non ne potrai più fare a meno!</p>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12 col-md-6 col-lg-6">
-                        <div class="my-btn-footer">
-                            <a class="w-50" href="https://www.apple.com/it/app-store/">
-                                <img class="img-fluid" src="https://thumbs.dreamstime.com/b/editorial-apple-app-store-download-banner-descriptionapple-inc-american-multinational-technology-company-headquartered-136704089.jpg" alt="Apple store">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-lg-6">
-                        <div class="my-btn-footer">
-                            <a class="w-50" href="https://play.google.com/store/apps?gl=IT&utm_source=emea_Med&utm_medium=hasem&utm_content=Oct2020&utm_campaign=Evergreen&pcampaignid=MKT-EDR-emea-it-1001280-Med-hasem-ap-Evergreen-Oct2020-Text_Search_SKWS%7CONSEM_kwid_43700007423260291&gclid=Cj0KCQiA54KfBhCKARIsAJzSrdpZVMgMWUnXTCF7pG5mmnpc0cxlcAaPgeFTXolIPv5VRokOHPIR8ysaAnjvEALw_wcB&gclsrc=aw.ds&pli=1">
-                                <img class="img-fluid mt-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5zOmjz3-B1-Yy99GaWjS61hDdkRjcgOs43w&usqp=CAU" alt="Google store" >
-                            </a>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+                <h2 class="text-center">Provalo anche su smartphone</h2>
+                <p class="text-center">Non ne potrai più fare a meno!</p>
+                <ul class="list-unstyled mb-0 d-flex justify-content-center">
+                    <li>
+                        <a href="https://www.apple.com/it/app-store/"><img
+                                src="https://thumbs.dreamstime.com/b/editorial-apple-app-store-download-banner-descriptionapple-inc-american-multinational-technology-company-headquartered-136704089.jpg"
+                                alt="Apple store" class="img-fluid m-1"></a>
+                    </li>
+                    <li>
+                        <a href="https://play.google.com/store/apps?gl=IT&utm_source=emea_Med&utm_medium=hasem&utm_content=Oct2020&utm_campaign=Evergreen&pcampaignid=MKT-EDR-emea-it-1001280-Med-hasem-ap-Evergreen-Oct2020-Text_Search_SKWS%7CONSEM_kwid_43700007423260291&gclid=Cj0KCQiA54KfBhCKARIsAJzSrdpZVMgMWUnXTCF7pG5mmnpc0cxlcAaPgeFTXolIPv5VRokOHPIR8ysaAnjvEALw_wcB&gclsrc=aw.ds&pli=1"
+                            class="text-white"><img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5zOmjz3-B1-Yy99GaWjS61hDdkRjcgOs43w&usqp=CAU"
+                                alt="Google store" class="img-fluid m-1"></a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    
-    
 
     <div class="container-wave">
-      <!-- img background wave -->
+        <!-- img background wave -->
     </div>
 
 </template>
 
 <script>
-    export default {
-        name: 'MobileAppComponent',
-    }
+export default {
+    name: 'MobileAppComponent',
+}
 </script>
 
 <style lang="scss" scoped>
- @import '../../assets/styles/general.scss';
- @import '../../assets/styles/partials/variables';
+@import '../../assets/styles/general.scss';
+@import '../../assets/styles/partials/variables';
 
 .container-wave {
     width: 100%;
-    background-image: url(../../../public/image/wave.svg);
+    background-image: url(image/wave.svg);
     background-repeat: no-repeat;
     background-size: cover;
     height: 15rem;
-    // position: relative;
 
 }
 
-.container-smathphone {
+.container-smartphone {
     margin-bottom: -15rem;
-    // position: absolute;
-    // top: -11rem;
-    // right: 0;
-    // left: 0;
-    // z-index: 100;
+}
+
+.phone {
+    width: 70%;
 }
 
 h2 {
     font-size: 3rem;
+    color: $orange;
 }
 
 p {
     font-size: 1.5rem;
+    color: $orange;
 }
 
 .my-btn-footer {
     padding: 2rem;
 }
-
-a {
-
-    img {
-        width: 100%;
-    }
-}
-
-
 </style>
--- src/components/header_partials/JumboComponent.vue
+++ src/components/header_partials/JumboComponent.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="bg-orange container-fluid">
-        <div class="container d-flex" height="465px">
+        <div class="container d-flex">
             <div class="row align-items-center">
                 <div class="col-sm-12 col-md-6 col-lg-6">
                     <img src="image/panino.png" alt="panino" class="img-fluid img-panino">
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6">
-                    <h1 class="text-white"><strong>Il tuo cibo preferito a portata di click!</strong> </h1>
-                    <p class="text-white"> <strong>Scegli il tuo piatto preferito e ordina direttamente da casa
+                <div class="col-sm-12 col-md-6 col-lg-6 position-relative z-index-2">
+                    <h1 class="text-dark"><strong>Il tuo cibo preferito a portata di click!</strong> </h1>
+                    <p class="text-dark"> <strong>Scegli il tuo piatto preferito e ordina direttamente da casa
                             tua!</strong></p>
+                    <div class="btn-jumbo">
+                        <a href="#" class="btn mybtn">Scopri i ristoranti</a>
+                    </div>
                 </div>
             </div>
-
         </div>
+    </div>
+
+    <div class="container-wave">
+        <!-- img background wave -->
     </div>
 
 
@@ -27,4 +33,32 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/styles/general.scss';
 @import '../../assets/styles/partials/variables';
+
+
+.bg-orange {
+    background-color: $orange;
+
+}
+
+.container {
+    height: 400px;
+}
+
+.img-panino {
+    position: relative;
+    z-index: 1;
+    transform: rotate(25deg);
+}
+
+.container-wave {
+    width: 100%;
+    background-image: url(image/wave-orange.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    z-index: -1;
+    height: 15rem;
+    transform: scaleY(-1);
+
+}
 </style>
