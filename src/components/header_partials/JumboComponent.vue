094dde981cb7b90fe93d87d3c363741d5033ba05
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-10 15:08:40 +0100

    jumbo

--- src/components/header_partials/JumboComponent.vue
+++ src/components/header_partials/JumboComponent.vue
@@ -1,15 +1,30 @@
 <template>
-    <h2>Jumbo</h2>
+    <div class="bg-orange container-fluid">
+        <div class="container d-flex" height="465px">
+            <div class="row align-items-center">
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <img src="image/panino.png" alt="panino" class="img-fluid img-panino">
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6">
+                    <h1 class="text-white"><strong>Il tuo cibo preferito a portata di click!</strong> </h1>
+                    <p class="text-white"> <strong>Scegli il tuo piatto preferito e ordina direttamente da casa
+                            tua!</strong></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
 </template>
 
 <script>
-    export default {
-        name: 'JumboComponent',
-    }
+export default {
+    name: 'JumboComponent',
+}
 </script>
 
 <style lang="scss" scoped>
- @import '../../assets/styles/general.scss';
- @import '../../assets/styles/partials/variables';
-
+@import '../../assets/styles/general.scss';
+@import '../../assets/styles/partials/variables';
 </style>
