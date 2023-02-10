c2c07494dd5b2739e364e86d695851069e4d3052
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-10 12:27:50 +0100

    header component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,27 +1,26 @@
 <template>
 
-    <MobileAppComponent/>
+    <MobileAppComponent />
 
     <div class="container">
-        <MapComponent/>
+        <MapComponent />
     </div>
 
 
     <h2>Footer Component</h2>
- </template>
- 
-<script>
-    import MobileAppComponent from './footer_partials/MobileAppComponent.vue';
-    import MapComponent from './footer_partials/MapComponent.vue';
+</template>
 
-     export default {
-        name: "FooterComponent",
-        components: { MobileAppComponent, MapComponent }
-    }
- </script>
- 
- <style lang="scss" scoped>
- @import '../assets/styles/general.scss';
- @import '../assets/styles/partials/variables';
- 
- </style>
+<script>
+import MobileAppComponent from './footer_partials/MobileAppComponent.vue';
+import MapComponent from './footer_partials/MapComponent.vue';
+
+export default {
+    name: "FooterComponent",
+    components: { MobileAppComponent, MapComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/general.scss';
+@import '../assets/styles/partials/variables';
+</style>
