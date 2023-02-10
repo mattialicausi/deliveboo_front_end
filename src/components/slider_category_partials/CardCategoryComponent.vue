c5b619842f7f0387d33f6b5a8014842f3222090d
mattialicausi <licausivitomattia@gmail.com>
2023-02-10 19:07:16 +0100

    aggiunto carousel

--- src/components/slider_category_partials/CardCategoryComponent.vue
+++ src/components/slider_category_partials/CardCategoryComponent.vue
@@ -1,24 +1,31 @@
 <template>
 
-<section class="outer-container-cards">
-    <div ref="box" class="cards-container" @mouseover="stopAutoplay" @mouseleave="autoplay">
+<!-- desktop carousel -->
+<section class=" d-flex justify-content-center">
+    
+    <vueper-slides class="no-shadow container" :visible-slides="6" :slide-ratio="1 / 6" :dragging-distance="70">
 
-        <!-- aggiungere link per ristoranti filtrati per categoria -->
+    <vueper-slide v-for="(category, i) in categories" :key="i" :image="category.image" />
+        
+    </vueper-slides>
 
-        <div class="col-2 flex-column justify-content-center align-items-center" v-for="(category, index) in categories" :key="index">
-            <img class="w-100 h-75" :src="category.image" :alt="category.name">
-            <h5 class="text-center">{{ category.name }}</h5>
-        </div>
-    </div>
 </section>
+
+
 
 </template>
 
 <script>
 import axios from 'axios';
 import {store} from '../../store';
+import { VueperSlides, VueperSlide } from 'vueperslides';
+import 'vueperslides/dist/vueperslides.css';
+
+
     export default {
         name: 'CardCategoryComponent',
+
+        components: {VueperSlides, VueperSlide},
 
         data() {
             return {
@@ -37,31 +44,10 @@ import {store} from '../../store';
                 });
             },
 
-            scrollRgt() {
-                const element = this.$refs.box;
-                    if (element != null) {
-                        element.scrollBy({
-                        left: 800,
-                        behavior: "smooth",
-                    });
-                }
-            },
-
-            autoplay() {
-                this.autoscroll = setInterval(() => {
-                    this.scrollRgt();
-                }, 2000);
-            },
-
-            stopAutoplay() {
-                clearInterval(this.autoscroll);
-                this.autoscroll = null;
-            },
         },
 
         mounted() {
             this.getCategories();
-            this.autoplay();
         },
     }
 </script>
@@ -70,33 +56,13 @@ import {store} from '../../store';
  @import '../../assets/styles/general.scss';
  @import '../../assets/styles/partials/variables';
 
- .outer-container-cards {
-  height: 50%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow-x: auto;
 
-  .cards-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    overflow-x: auto;
+ @media screen and (max-width: 576px) {
+  .my-container {
+   max-width: 450px;
   }
 }
 
-//style scrollbar slider
-::-webkit-scrollbar {
-  height: 7px;
-}
-
-// Handle
-::-webkit-scrollbar-thumb {
-  -webkit-border-radius: 2px;
-  background-color: $orange;
-  border-radius: 2px;
-  width: 5%;
-}
 
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from './router'
-
-
-
 import './assets/styles/general.scss';
+
+
+
+
 
 
 
