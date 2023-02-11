afdee1a36463c2f6b4cd61a71406896a9841b498
daniel iabanji <daniel.iabanji@yahoo.com>
2023-02-11 14:58:05 +0100

    card ristorante

--- src/store.js
+++ src/store.js
@@ -6,23 +6,28 @@ export const store = reactive({
   apiBaseUrl: "http://127.0.0.1:8000/api",
   imgBasePath: "http://127.0.0.1:8000/storage/",
 
+  data: {
+    params: {
+      categoryFilter: '',
+    }
+  },
   categories: [],
   types: [],
   restaurants: [],
 
-  
-  getCategories: function(){
-    axios.get(`${this.apiBaseUrl}/categories`).then((res)=>{
+
+  getCategories: function () {
+    axios.get(`${this.apiBaseUrl}/categories`).then((res) => {
       this.categories = res.data.categories;
       console.log(res.data.categories)
-  })
+    })
   },
 
-  getTypes: function(){
-    axios.get(`${this.apiBaseUrl}/types`).then((res)=>{
+  getTypes: function () {
+    axios.get(`${this.apiBaseUrl}/types`).then((res) => {
       this.types = res.data.types;
       console.log(res.data.types)
-  })
+    })
   },
 
 
