1882cbaf7b188a5f7ab798ce75da6aa4374389e4
BiagioButta <biagiob940@gmail.com>
2023-02-11 03:02:06 +0100

    swmo template restaurant

--- src/components/CardRestaurantComponent.vue
+++ src/components/CardRestaurantComponent.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="row">
+    <!-- <div class="row">
 
         <div class="col">
             <div v-for="(categories, i) in categories">{{categories.id === restaurant.id ? categories.name : ''}}</div>
@@ -20,36 +20,130 @@
             </div>
         
         </div>
+    </div> -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            
+                <select name="category" id="category" v-model="forCategory">
+                    <option value="">Category</option>
+                    <option v-for="(category, index) in store.categories" :key="index" :value="`${category.id}`">{{ category.name }}</option>
+                </select>
+
+            </div>
+        </div>
     </div>
+
+
+    <div class="container-card">
+           
+
+            <div class="container mb-5" v-for="(restaurant, index) in restaurants" :key="index">
+            
+            
+                <div class="row align-items-center">
+                
+                    <div class="col card restaurantImg">
+                        <img :src="restaurant.image" alt="restaurant.name">
+                    </div>
+
+                    <div class="col">
+                    
+                        <h3 class="fw-bold">{{ restaurant.restaurant_name }}</h3>
+                        <p>
+                            <!-- <div>
+                                <span v-for="n in maxRating" :key="n" :class="'fa fa-star'"></span>
+                            </div> -->
+                            <span v-if="restaurant.rating"><i class="fa-solid fa-star"></i> {{ restaurant.rating }}/5</span>
+                            <!-- <span v-if="!restaurant.rating">Nessun voto</span> -->
+                        </p>
+                        <p>{{ truncateText(restaurant.description) }}</p>
+                    
+                    </div>
+
+                    <div class="col">
+                    
+                        <p>
+                            <span v-if="restaurant.min_price_order">{{ restaurant.min_price_order }} &euro;</span>
+                            <span v-if="!restaurant.min_price_order">Nessun prezzo minimo per l'ordine</span>
+                        </p>
+                        <p>
+                            <span v-if="restaurant.delivery_price">Costo consegna {{ restaurant.delivery_price }} &euro;</span>
+                            <span v-if="!restaurant.delivery_price">Consegna gratuita</span>
+                        
+                        </p>
+                    
+                    </div>
+  
+                </div>
+            </div>
+
+        </div>
           
 </template>
 
 <script>
 
-// import axios from 'axios';
+import axios from 'axios';
 import {store} from '../store';
+
 
     export default {
         name : "CardRestaurantComponent",
+        
+
+        
 
         data() {
             return {
                 store,
+                restaurants: [],
+                maxLength: 25,
+                maxRating: 5,
+                rating: 0
             }
         },
 
-        props: ['restaurant', 'categories'],
-
         methods: {
-           
+            getRestaurants() {
+                axios.get(`${this.store.apiBaseUrl}/restaurants`).then((res) => {
+                    this.restaurants = res.data.results;
+                    
+                });
+            },
+
+            truncateText(text) {
+                if (text.length > this.maxLength) {
+                    return text.substr(0, this.maxLength) + "...";
+                }
+                return text;
+            },
+
+            setRating(rating) {
+            this.rating = rating;
+    }
+
         
         },
         mounted() {
-            
+            this.getRestaurants();
         },
     }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/styles/general.scss';
+@import '../assets/styles/partials/variables';
+
+
+.restaurantImg{
+
+    img{
+        width: 100%;
+        aspect-ratio: 1.8;
+    }
+}
+
 
 </style>
--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+import axios, { Axios } from 'axios';
 import { reactive } from "vue";
 
 export const store = reactive({
@@ -5,7 +6,24 @@ export const store = reactive({
   apiBaseUrl: "http://127.0.0.1:8000/api",
   imgBasePath: "http://127.0.0.1:8000/storage/",
 
+  categories: [],
+  types: [],
+  restaurants: [],
+
   
+  getCategories: function(){
+    axios.get(`${this.apiBaseUrl}/categories`).then((res)=>{
+      this.categories = res.data.categories;
+      console.log(res.data.categories)
+  })
+  },
+
+  getTypes: function(){
+    axios.get(`${this.apiBaseUrl}/types`).then((res)=>{
+      this.types = res.data.types;
+      console.log(res.data.types)
+  })
+  },
 
 
 });
