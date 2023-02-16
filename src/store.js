cf68ad22b1789a8dc3284cf151bca95e812fd422
StefaniaSperani <thedarkcry@hotmail.it>
2023-02-16 10:22:48 +0100

    modify img categories

--- src/store.js
+++ src/store.js
@@ -1,106 +1,111 @@
-import axios, { Axios } from 'axios';
+import axios, { Axios } from "axios";
 import { reactive } from "vue";
 
 export const store = reactive({
-
   apiBaseUrl: "http://127.0.0.1:8000/api",
   imgBasePath: "http://127.0.0.1:8000/storage/",
 
   data: {
     params: {
-      categoryFilter: '',
-
-    }
+      categoryFilter: "",
+    },
   },
 
   categorySlider: [
     {
-      name: 'pizza',
-      img: '/image/img-category/pizza-category.webp',
+      name: "pizza",
+      img: "/image/img-category/pizza.png",
       id: 2,
     },
     {
-      name: 'americano',
-      img: '/image/img-category/hamburger-category.webp',
+      name: "americano",
+      img: "/image/img-category/hamburger.png",
       id: 5,
     },
     {
-      name: 'cinese',
-      img: '/image/img-category/cinese-category.webp',
+      name: "cinese",
+      img: "/image/img-category/cinese.png",
       id: 1,
     },
     {
-      name: 'italiano',
-      img: '/image/img-category/italiano-category.webp',
+      name: "italiano",
+      img: "/image/img-category/italiano.png",
       id: 3,
     },
     {
-      name: 'kebab',
-      img: '/image/img-category/kebab-category.webp',
+      name: "kebab",
+      img: "/image/img-category/kebab.png",
       id: 4,
     },
     {
-      name: 'fritti',
-      img: '/image/img-category/americano-category.webp',
+      name: "fritti",
+      img: "/image/img-category/americano.png",
       id: 14,
     },
     {
-      name: 'poké',
-      img: '/image/img-category/poke-category.webp',
+      name: "poké",
+      img: "/image/img-category/poke.png",
       id: 7,
     },
     {
-      name: 'africano',
-      img: '/image/img-category/africano-category.png',
+      name: "africano",
+      img: "/image/img-category/africano.png",
       id: 8,
     },
     {
-      name: 'colazione',
-      img: '/image/img-category/colazione-category.png',
+      name: "colazione",
+      img: "/image/img-category/colazione.png",
       id: 16,
     },
   ],
 
   categoryAllRestaurant: [
     {
-      name: 'pizza',
-      img: '/image/img-category/pizza-category.webp',
+      name: "pizza",
+      img: "/image/img-category/pizza.png",
       id: 2,
     },
     {
-      name: 'americano',
-      img: '/image/img-category/hamburger-category.webp',
+      name: "americano",
+      img: "/image/img-category/hamburger.png",
       id: 5,
     },
     {
-      name: 'cinese',
-      img: '/image/img-category/cinese-category.webp',
+      name: "cinese",
+      img: "/image/img-category/cinese.png",
       id: 1,
     },
     {
-      name: 'italiano',
-      img: '/image/img-category/italiano-category.webp',
+      name: "italiano",
+      img: "/image/img-category/italiano.png",
       id: 3,
     },
     {
-      name: 'kebab',
-      img: '/image/img-category/kebab-category.webp',
+      name: "kebab",
+      img: "/image/img-category/kebab.png",
       id: 4,
     },
     {
-      name: 'poké',
-      img: '/image/img-category/poke-category.webp',
+      name: "poké",
+      img: "/image/img-category/poke.png",
       id: 7,
     },
     {
-      name: 'kebab',
-      img: '/image/img-category/kebab-category.webp',
+      name: "kebab",
+      img: "/image/img-category/kebab.png",
       id: 4,
     },
-
-
+    {
+      name: "africano",
+      img: "/image/img-category/africano.png",
+      id: 8,
+    },
+    {
+      name: "colazione",
+      img: "/image/img-category/colazione.png",
+      id: 16,
+    },
   ],
-
 
   categories: [],
   types: [],
@@ -113,7 +118,7 @@ export const store = reactive({
   shoppingCart: [],
   cartData: [],
   cart: [],
-  final_price: '',
+  final_price: "",
 
   paid_status: null,
 
@@ -121,23 +126,19 @@ export const store = reactive({
 
   cartShow: false,
 
-
-
-
   getCategories: function () {
     axios.get(`${this.apiBaseUrl}/categories`).then((res) => {
       this.categories = res.data.categories;
       // console.log(res.data.categories)
-    })
+    });
   },
 
   getTypes: function () {
     axios.get(`${this.apiBaseUrl}/types`).then((res) => {
       this.types = res.data.types;
       // console.log(res.data.types)
-    })
+    });
   },
-
 
   // funzione per il totale del carrello
   cartTotal() {
@@ -155,7 +156,5 @@ export const store = reactive({
     });
     const hiddenElements = document.querySelectorAll(".hidden");
     hiddenElements.forEach((element) => observer.observe(element));
-  }
-
-
+  },
 });
